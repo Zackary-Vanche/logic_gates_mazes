c26c44d0ec76cdbc2900695dc72f3cea1cf623ff
Zackary-Vanche <zackary.vanche@gmail.com>
2022-03-05 09:23:38 +0100

    Changing image creation.
    It has no incidence on the game.

--- logic_gates_mazes_python_code/image_fusion.py
+++ logic_gates_mazes_python_code/image_fusion.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     del racine[-1]
     racine.append('images')
     racine = '/'.join(racine)
-    for size in [[1350, 768], [1920, 1001], [1920, 1055]]:
+    for size in [[1346, 668], [1920, 1001], [1920, 1055]]:
         try:
             print(size)
             WIDTH, HEIGHT = size
@@ -21,15 +21,15 @@ if __name__ == "__main__":
             for n in sorted(dico.keys()):
                 file_list.append(dico[n])
                 
-            l_img_h = []
-            for i in range(4):
-                l = file_list[4*i:4*i+4]
-                l = [cv2.imread(file) for file in l]
-                im_h = cv2.hconcat(l)
-                l_img_h.append(im_h)
-            img = cv2.vconcat(l_img_h)
+            # l_img_h = []
+            # for i in range(4):
+            #     l = file_list[4*i:4*i+4]
+            #     l = [cv2.imread(file) for file in l]
+            #     im_h = cv2.hconcat(l)
+            #     l_img_h.append(im_h)
+            # img = cv2.vconcat(l_img_h)
             
-            cv2.imwrite('images/concat_levels_{}.jpg'.format(string), img)
+            # cv2.imwrite('images/concat_levels_{}.jpg'.format(string), img)
             
             cv2.imwrite('images/concat_line_levels_{}.jpg'.format(string), cv2.hconcat([cv2.imread(file) for file in file_list]))
         except:
