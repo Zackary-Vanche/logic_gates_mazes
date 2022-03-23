8b98a0b7ce7ff1e0ebe7eba0853c88f55cd30e2c
Zackary-Vanche <zackary.vanche@gmail.com>
2022-03-23 19:37:54 +0100

    compiling

--- logic_gates_mazes_python_code/logic_gates_mazes_main.py
+++ logic_gates_mazes_python_code/logic_gates_mazes_main.py
@@ -6,7 +6,7 @@ Created on Fri Jan 21 16:02:21 2022
 """
 # from numba import njit
 
-from sys import exit as sys_exit
+# from sys import exit as sys_exit
 from Game import Game
 from pyautogui import size as pyautogui_size
 
@@ -14,17 +14,18 @@ if __name__ == "__main__":
 
     TOTAL_SIZE = pyautogui_size()
 
-    Game().play() 
+    Game().play()
 
-    # Game(save_image = True).play() 
+    # Game(save_image = True).play()
 
     # Game.play(WINDOW_SIZE = TOTAL_SIZE, save_image = True)
 
-    sys_exit()
+    # sys_exit()
 
     # À lancer dans le Anaconda powershell :
     # pyinstaller C:\Users\utilisateur\Documents\github\logic_gates_mazes\logic_gates_mazes_python_code\logic_gates_mazes_main.py --distpath C:\Users\utilisateur\Documents\github\logic_gates_mazes\logic_gates_mazes_executable --icon=C:\Users\utilisateur\Documents\github\logic_gates_mazes\icone.ico
 
     # Si on ne veut qu'un seul fichier au lieu d'un dossier
     # on ajoute -F
-    # En ne prenant qu'un seul fichier, l'application est beaucoup plus longue à se lancer
+    # En ne prenant qu'un seul fichier,
+    # l'application est beaucoup plus longue à se lancer
