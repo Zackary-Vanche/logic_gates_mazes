46efe26895360c7c646831fcfb93abbc429c5bfc
Zackary-Vanche <zackary.vanche@gmail.com>
2024-07-28 22:04:53 +0200

    small change in front end

--- src/levels/level_bis_repetita.py
+++ src/levels/level_bis_repetita.py
@@ -4,7 +4,7 @@ from Door import Door
 from Room import Room
 from Maze import Maze
 from Levels_colors_list import Levels_colors_list
-
+import numpy as np
 
 def level_bis_repetita():
     S0 = Switch(name='S0')
@@ -25,38 +25,47 @@ def level_bis_repetita():
               switches=[S0, S1, S2])
 
     R0 = Room(name='R0',
-              position=[0, 0, 3, 3],
+              position=[0, 4, 3, 3],
               switches_list=[S0])
     R1 = Room(name='R1',
-              position=[2, 4, 1, 1],
+              position=[4, 0, 3, 3],
               switches_list=[S1])
     R2 = Room(name='R2',
-              position=[4, 4, 3, 3],
+              position=[5, 5, 2, 2],
               switches_list=[S2])
     RE = Room(name='RE',
-              position=[4, 2, 1, 1],
+              position=[3+(1-np.sqrt(2))/2, 3+(1-np.sqrt(2))/2, np.sqrt(2), np.sqrt(2)],
               is_exit=True)  # E pour exit ou end
+    
+    rp = 0.4
 
     D0 = Door(two_way=False,
               tree=T0,
               room_departure=R0,
               room_arrival=R1,
-              relative_position=0.7)
+              relative_position=rp,
+              relative_departure_coordinates=[1/6, 1/6],
+              relative_arrival_coordinates=[1/6, 1/6])
     D1 = Door(two_way=False,
               tree=T1,
               room_departure=R1,
               room_arrival=R2,
-              relative_position=0.35)
+              relative_position=rp,
+              relative_departure_coordinates=[5/6, 5/6],
+              relative_arrival_coordinates=[3/4, 1/4])
     D2 = Door(two_way=False,
               tree=T2,
               room_departure=R2,
               room_arrival=R0,
-              relative_position=0.5)
+              relative_position=rp,
+              relative_departure_coordinates=[1/4, 3/4],
+              relative_arrival_coordinates=[5/6, 5/6])
     D3 = Door(two_way=False,
               tree=T3,
               room_departure=R2,
               room_arrival=RE,
-              relative_position=0.7)
+              relative_position=rp,
+              relative_departure_coordinates=[1/4, 1/4])
 
     level = Maze(start_room_index=0,
                  exit_room_index=-1,
